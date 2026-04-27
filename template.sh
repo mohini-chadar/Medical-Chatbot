515045db65f1b85cb9d9f9feda2a9bdbd9dc8194
Mohini <mohini.chadar46@gmail.com>
2026-04-28 01:06:17 +0530

    folder structure added

---
+++ template.sh
@@ -0,0 +1,12 @@
+mkdir -p src
+mkdir -p research
+
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.ipynb
+touch requirements.txt
+
+echo "Directory and files created successfully."
