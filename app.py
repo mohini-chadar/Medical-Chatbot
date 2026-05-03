e3d2393e2b6c3a127c411265714df11d2e5ef7c3
Mohini <mohini.chadar46@gmail.com>
2026-05-03 23:18:48 +0530

    solved a bug

--- app.py
+++ app.py
@@ -11,10 +11,48 @@ import os
 
 app = Flask(__name__)
 
+load_dotenv()
+
+PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+
+os.environ["PINECONE_API_KEY"] = PINECONE_API_KEY
+os.environ["GROQ_API_KEY"] = GROQ_API_KEY
+
+embeddings = download_hugging_face_embeddings()
+
+index_name = "medical-chatbot"
+docsearch = PineconeVectorStore.from_existing_index(
+    index_name=index_name,
+    embedding=embeddings
+)
+
+retriver = docsearch.as_retriever(search_type="similarity", search_kwargs={"k":3})
+
+chatModel = ChatGroq(model="llama-3.3-70b-versatile")
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", system_prompt),
+        ("human", "{input}"),
+    ]
+)
+
+question_answer_chain = create_stuff_documents_chain(chatModel, prompt)
+rag_chain = create_retrieval_chain(retriver, question_answer_chain)
+
 
 @app.route("/")
 def index():
     return render_template('chat.html')
+
+@app.route("/get", methods=["GET", "POST"])
+def chat():
+    msg = request.form["msg"]
+    input = msg
+    print(input)
+    response = rag_chain.invoke({"input": msg})
+    print("Response : ", response["answer"])
+    return str(response["answer"])
 
 
 if __name__ == '__main__':
